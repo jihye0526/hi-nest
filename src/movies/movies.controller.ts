be007fb4ee3b12f41d0c0b171087c76e6fe4336a
jihye0526 <samtafe97@naver.com>
2023-07-30 15:06:07 +0900

    More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,14 +8,22 @@ export class MoviesController {
         return "This will return all movies";
     }
 
+    // search 부분이 getOne 보다 밑에 있으면 NestJS는 search를 id로 판단
+    // Query는 Query Parameter를 받아올 때 사용 (예, /users?id=123)
+    @Get("search")
+    serach(@Query("year") searchingYear: string){
+        return `We are searching for a movie made after ${searchingYear}`;
+    }
+
+    // Param은 Path Variable을 받아올 때 사용 (예, /users/123)
     @Get('/:id')
     getOne(@Param("id") movieId: string){
         return `This will return one movie with the id : ${movieId}`;
     }
 
     @Post()
-    create(){
-        return "This will create a movie";
+    create(@Body() movieData){
+        return movieData;
     }
 
     @Delete('/:id')
@@ -24,8 +32,10 @@ export class MoviesController {
     }
     
     @Patch('/:id')
-    patch(@Param('id') movieId: string){
-        return `This will patch with the id ${movieId}`;
+    patch(@Param('id') movieId: string, @Body() updateData){
+        return {
+            updateMovie: movieId,
+            ...updateData
+        };
     }
-
 }
