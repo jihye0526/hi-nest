4e09da4ba62865f12df3cfac442b3c126a6ee118
jihye0526 <samtafe97@naver.com>
2023-08-20 13:09:04 +0900

    4.1 Testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    //main.ts에 있는 걸 동일하게 pipe를 통해 사용해줘야 transfrom과 같은 동작들이 정상적으로 동작함(테스트에서도 실제 어플리케이션의 환경을 그대로 적용해줘야함)
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,    
+      forbidNonWhitelisted: true,
+      transform: true,
+    }));
     await app.init();
   });
 
@@ -46,5 +52,23 @@ describe('AppController (e2e)', () => {
         .delete("/movies")
         .expect(404);
     });
+  });
+
+  describe("/movies/:id", () => {
+    it("GET 200", () => {
+      return request(app.getHttpServer())
+        .get("/movies/1")
+        .expect(200);
+    });
+
+    it("GET 404", () => {
+      return request(app.getHttpServer())
+        .get("/movies/999")
+        .expect(404);
+    });
+
+    it.todo("DELETE");
+
+    it.todo("PATCH");
   });
 });
